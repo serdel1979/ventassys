87b468bbb4a4600f1b7db327a76bc5206b312ad3
Sergio De Luca <serg.r.deluca@gmail.com>
2023-12-11 00:31:50 -0300

    sistema de ventas codeigniter 4

---
+++ app/Views/includes/footer.php
@@ -0,0 +1,20 @@
+<footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Sergio <?= date('Y'); ?></div>
+                            <div>
+                                <a href="#">Privacy Policy</a>
+                                &middot;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="js/scripts.js"></script>
+        <script src="js/simple-datatables.min.js"></script>
+        <script src="js/datatables-simple-demo.js"></script>
+    </body>
+</html>
---
+++ app/Views/page.php
@@ -0,0 +1,5 @@
+<main>
+     <div class="container-fluid px-4">
+        <h5>Hola mundo</h5>
+    </div>
+</main>
